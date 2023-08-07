80660789d50738000a40b0718fd4b97af57ff694
hebamesluh1 <hebamesluh@gmail.com>
2023-08-07 20:38:47 +0300

    layout done 🫡✅

---
+++ components/index.ts
@@ -0,0 +1,11 @@
+import Hero from "./Hero";
+import CustomButton from './CustomButton/index';
+import Navbar from './Navbar/index';
+import Footer from './Footer/index';
+
+export {
+    Hero,
+    CustomButton,
+    Navbar,
+    Footer
+}
